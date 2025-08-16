9d23189cc97f0d5e0c0576a9e8a263f48af410b8
samarth <samarth@DESKTOP-PCHIK9I>
2025-08-16 18:17:03 +0530

    Added a code in TC1 and added TestCase2 as a TESTER.

--- src/test/java/gitPackage/gitTestCase1.java
+++ src/test/java/gitPackage/gitTestCase1.java
@@ -5,6 +5,7 @@ public class gitTestCase1 {
 	public static void main(String[] args) {
 
 		System.out.println("This is a git TestCase 1, by QALEAD.");
+		System.out.println("Added a new code as a TESTER");
 		
 	}
 
---
+++ src/test/java/gitPackage/gitTestCase2.java
@@ -0,0 +1,11 @@
+package gitPackage;
+
+public class gitTestCase2 {
+
+	public static void main(String[] args) {
+
+	System.out.println("Added TestCase2 as a TESTER");
+		
+	}
+
+}
